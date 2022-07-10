e4803a1b43401d010fa186f925835743abccd5a6
Martin Emanuel <martinemanuel93@gmail.com>
2022-07-10 10:37:22 -0300

    redirects

--- src/pages/Page01.tsx
+++ src/pages/Page01.tsx
@@ -4,15 +4,15 @@ import { Environment, OrbitControls, Plane, SpotLight } from '@react-three/drei'
 import { MathUtils } from 'three';
 import CameraVintage from '../3dComponents/CameraVintage';
 
-export default function Page01() {
+const canvasCss = { margin: 0, height: '90vh', width: '100vw', background: 'gray' };
 
+export default function Page01() {
   return (
-    <Canvas style={{ margin: 0, height: '90vh', width: '100vw', background: 'gray' }}
-      camera={{ position: [0, 6, 5] }}>
+    <Canvas style={canvasCss} camera={{ position: [0, 6, 5] }}>
       <ambientLight intensity={1.4} />
       <CameraVintage scale={15} />
       <Environment background preset='studio' />
-      <OrbitControls /> 
+      <OrbitControls />
     </Canvas>
 
   );
