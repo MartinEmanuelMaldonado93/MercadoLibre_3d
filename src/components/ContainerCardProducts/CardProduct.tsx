2dc2ecf8e417d458875ab5b5da9f8170e7c7ec88
Martin Emanuel <martinemanuel93@gmail.com>
2022-11-25 18:48:45 -0300

    fix useEffect side efects

--- src/components/ContainerCardProducts/CardProduct.tsx
+++ src/components/ContainerCardProducts/CardProduct.tsx
@@ -12,114 +12,107 @@ type props = {
 const css = createCss(styles);
 
 function CardPrice({ imgUrl, price, description, freeShip }: props) {
-  const [pos, setPos] = useState({ x: 0, y: 0 });
+  const [isOnMove, setEnter] = useState<boolean>(false);
   const y = useRef<number>();
   const x = useRef<number>();
-  const [enterFlag, setEnter] = useState<boolean>();
-  const curr = useRef({ currX: 0, currY: 0 });
   const card = useRef<HTMLDivElement>();
-  const RAQ_ID = useRef<number>();
-  const previousTimeRef = useRef<number>();
-  
-  let currX = 0, currY = 0;
+  const RAF_ID = useRef<number>();
+  const previousTime = useRef<number>();
+  const DEG = 35;
 
+  let currX = x.current - 0.5, currY = y.current - 0.5;
   const animate = (time: number) => {
-    if (previousTimeRef.current && enterFlag) {
-      const delta = time - previousTimeRef.current;
-      console.log("running ");
-      // Lerp
-      currX = (currX + (x.current - currX) * 0.15);
-      currY = (currY + (y.current - currY) * 0.15);
-      // currX = x.current;
-      // currY = y.current;
-
-      const { innerWidth, innerHeight } = window;
-      const rotX = ((currY / innerHeight) * -2) + 1;
-      const rotY = ((currX / innerWidth) * 2) - 1;
-
-      card.current.style.setProperty("--rotX", String(rotX))
-      card.current.style.setProperty("--rotY", String(rotY))
-
-      // Light on the card 
-      const rect = card.current.getBoundingClientRect();
-      const lightX = x.current - rect.left;
-      const lightY = y.current - rect.top;
-      card.current.style.setProperty("--mouseX", `${lightX}px`)
-      card.current.style.setProperty("--mouseY", `${lightY}px`)
-      // shadow
-      // console.log(rect)
-      const halfHeight = rect.height / 2;// 163 ---326
-      const halfWidth = rect.width / 2;// 95 ---- 190
-      const shadowY = (halfHeight - lightY) / 100;
-      const shadowX = (halfWidth - lightX) / 100;
-      card.current.style.setProperty("--x-shadow", `${shadowX * 4}px`)
-      card.current.style.setProperty("--y-shadow", `${shadowY * 4}px`)
+    let delta = (time - previousTime.current) / 1000;
+    // if (delta > 1) {
+    //   console.log(` delta ${delta} `);// each frame 0.02~ aprox.
+    //   delta = 0;
+    // }
+    if (!previousTime.current) {
+      previousTime.current = time;
     }
-    previousTimeRef.current = time;
-    RAQ_ID.current = requestAnimationFrame(animate);
-  }
+    if (!isOnMove) {
+      // console.log("leave")
+      setTimeout(() => {
+        console.log("cancel 2s ", isOnMove);
+        cancelAnimationFrame(RAF_ID.current);
+      }, 2000);
+      // cancelAnimationFrame(RAF_ID.current);
+      return;
+    }
+    // console.log("running", x.current, y.current)
 
+    // Lerp
+    currX = (currX + (x.current - currX) * 0.15);// static 15%
+    currY = (currY + (y.current - currY) * 0.15);
+    // currX = (currX + (x.current - currX) * (delta * 10));// 0.02 smooth
+    // currY = (currY + (y.current - currY) * (delta * 10));
+    // currX = x.current;
+    // currY = y.current;
+    // Rotation
+    // const rotX = ((currY / innerHeight) * -2) + 1; // works
+    // const rotY = ((currX / innerWidth) * 2) - 1;// works
+    const rotX = ((currY / window.innerHeight) * -2) + 1;// 0.012 deg 
+    const rotY = ((currX / window.innerWidth) * 2) - 1;
+    // console.log(`rotx${rotX} roty${rotY}`);
+    // console.log(`sigo prendido ajaajaa`);
+    card.current.style.setProperty("--rotX", `${rotX}`)
+    card.current.style.setProperty("--rotY", `${rotY}`)
+
+    // Light on the card 
+    const rectCard = card.current.getBoundingClientRect();
+    const lightX = x.current - rectCard.left;
+    const lightY = y.current - rectCard.top;
+    card.current.style.setProperty("--mouseX", `${lightX}px`)
+    card.current.style.setProperty("--mouseY", `${lightY}px`)
+
+    // shadow
+    const halfHeight = rectCard.height / 2;// 163 ---326
+    const halfWidth = rectCard.width / 2;// 95 ---- 190
+    const shadowY = (halfHeight - lightY) / 100;
+    const shadowX = (halfWidth - lightX) / 100;
+    card.current.style.setProperty("--x-shadow", `${shadowX * 4}px`)
+    card.current.style.setProperty("--y-shadow", `${shadowY * 4}px`)
+
+    RAF_ID.current = requestAnimationFrame(animate);
+    previousTime.current = time;
+  }
+  // USE_EFFECT
   useEffect(() => {
     console.count("useEfect ");
-    RAQ_ID.current = requestAnimationFrame(animate);
 
-    return function cleanUp() {
-      cancelAnimationFrame(RAQ_ID.current);
+    RAF_ID.current = requestAnimationFrame(animate);
+
+    return () => {
+      console.log("cleanUp")
+      // cancelAnimationFrame(RAF_ID.current);
+      // setTimeout(() => {
+      //   console.log("cleanUp")
+      //   cancelAnimationFrame(RAF_ID.current);
+
+      // }, 2000);
     }
-  }, [enterFlag]);
-  // let currX = 0, currY = 0;
-  // useEffect(() => {
-  //   // console.count();
-  //   // let { currX, currY } = curr.current;
-  //   // Lerp
-  //   // currX = (currX + (pos.x - currX) * 0.15);
-  //   // currY = (currY + (pos.y - currY) * 0.15);
-  //   currX = pos.x;
-  //   currY = pos.y;
-
-  //   const { innerWidth, innerHeight } = window;
-  //   const rotX = ((currY / innerHeight) * -2) + 1;
-  //   const rotY = ((currX / innerWidth) * 2) - 1;
-
-  //   card.current.style.setProperty("--rotX", String(rotX))
-  //   card.current.style.setProperty("--rotY", String(rotY))
-
-  //   // Light on the card 
-  //   const rect = card.current.getBoundingClientRect();
-  //   const lightX = pos.x - rect.left;
-  //   const lightY = pos.y - rect.top;
-  //   card.current.style.setProperty("--mouseX", `${lightX}px`)
-  //   card.current.style.setProperty("--mouseY", `${lightY}px`)
-  //   // shadow
-  //   // console.log(rect)
-  //   const halfHeight = rect.height / 2;// 163 ---326
-  //   const halfWidth = rect.width / 2;// 95 ---- 190
-  //   const shadowY = (halfHeight - lightY) / 100;
-  //   const shadowX = (halfWidth - lightX) / 100;
-  //   card.current.style.setProperty("--x-shadow", `${shadowX * 4}px`)
-  //   card.current.style.setProperty("--y-shadow", `${shadowY * 4}px`)
-
-  //   return () => {
-  //     // cancelAnimationFrame(1);
-  //   };
-  // }, [pos]);
+  }, [isOnMove]);
 
   function onMouseMove(event: any) {
-    let e = event.touches ? event.touches[0] : event;
-    // setPos({ x: e.clientX, y: e.clientY });
+    const e = event.touches ? event.touches[0] : event;
+
+    if (!isOnMove) { // first time is false
+      setEnter(true);
+      return;
+    }
     x.current = e.clientX;
     y.current = e.clientY;
-    if (!enterFlag) {
-      setEnter(true);
-    }
-    // let x = e.clientX;
-    // let y = e.clientY;
-    // runAnimation(x, y);
+
   }
+
   function onLeave() {
-    console.log(" chau ")
-    setEnter(false);
+    //todo: reset function
+    console.log(`leave ${isOnMove}`)
+    if (isOnMove) {
+      setEnter(false);
+    }
   }
+
   return (
     <div className={css("card")}
       ref={card}
